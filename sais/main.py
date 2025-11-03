91a0b142a4413bebe6bc400c4deca8279e756394
Saidansafxon <saida01021987@gmail.com>
2025-11-03 20:45:02 +0500

    Add files via upload

---
+++ sais/main.py
@@ -0,0 +1,29 @@
+def sa(son1: int,son2: int,son3: int)->float:
+    average=(son1+son2+son3)/3
+    return average
+
+a,b,c=int(input("Birinchi sonni kiriting: ")),int(input("Ikkinchi sonni kiriting")),int(input("Uchinchi sonni kiritng"))
+
+print(sa(a,b,c))
+
+def reverse_text(text: str) -> str:
+    return text[::-1]
+
+sa1 = input("Matn kiriting: ")
+print(reverse_text(sa1))
+
+#lis1=[43,46,235,7356,312,65]
+#def sa2():
+ #   return max(list1)
+
+#print(sa2())
+
+def da()->str:
+    sa3=input("Ismingizni kiriting: ")
+    return text(sa3 )
+
+def yosh()->int:
+    sa4=int(input("Yoshingizni kiriting: "))
+    return text(sa4)
+
+print(da,yosh)
